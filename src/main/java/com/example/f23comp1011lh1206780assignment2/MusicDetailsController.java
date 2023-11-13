4a9da5382b57e92254ccea7a6f167dfcd48cfef2
fconiglione <122475578+fconiglione@users.noreply.github.com>
2023-11-13 16:07:46 -0500

    created the SceneBuilder outline for the Music Details

--- src/main/java/com/example/f23comp1011lh1206780assignment2/MusicDetailsController.java
+++ src/main/java/com/example/f23comp1011lh1206780assignment2/MusicDetailsController.java
@@ -1,4 +1,38 @@
 package com.example.f23comp1011lh1206780assignment2;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class MusicDetailsController {
+
+    @FXML
+    private Label albumNameLabel;
+
+    @FXML
+    private Label artistNameLabel;
+
+    @FXML
+    private ImageView musicImageVIew;
+
+    @FXML
+    private Label releaseDateLabel;
+
+    @FXML
+    private Label songNameLabel;
+
+    @FXML
+    private Label trackNameLabel;
+
+    @FXML
+    void goBackButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playOnSpotifyButton(ActionEvent event) {
+
+    }
+
 }
